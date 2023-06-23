4fea1570acd3aecb425e77f3f05469c72177dbb2
Christian Wittwer <christianwittwer@Christians-MacBook-Air.local>
2023-06-23 11:31:34 -0600

    Advanced Mongo

---
+++ Week 4/mongo-advanced/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const model = require("./model");
+
+
+const app = express();
+const port = 8080;
+app.use(express.urlencoded({extended:false}));
+
+
+app.get("/movies", function(req, res) {
+    var queryParams = {};
+    console.log(req.query);
+  
+
+
+    // custom query creation
+    for (key in req.query) {
+        if (key == "profit_lte") {
+            queryParams.profit["$lte"] = req.query[key]
+        }
+        if (key == "profit_gte") {
+            queryParams.profit["$gte"] = req.query[key]
+        }
+    }
+
+    // queryParams.profit = minMaxProfit;
+    //console.log(minMaxProfit);
+    console.log(queryParams);
+
+    
+
+    model.Movie.find(queryParams).then(function(movies) {
+        res.send(movies);
+    })
+})
+
+app.post("/movies", function(req, res) {
+    const newMovie = new model.Movie({
+        title: req.body.title,
+        production_cost: req.body.production_cost,
+        profit: req.body.profit,
+        director: req.body.director
+    })
+
+    newMovie.save().then(function() {
+        res.status(201).send("Movie saved.");
+    }).catch(function() {
+        res.status(422).send("Error saving movie.");
+    })
+})
+
+app.listen(port, function() {
+    console.log(`Running server on port ${port}...`);
+});
