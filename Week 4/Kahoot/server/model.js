b0e115fe1d61bcbe53d199bf8bd7cc55757490dc
Christian Wittwer <christianwittwer@Christians-MacBook-Air.local>
2023-12-15 09:04:40 -0700

    Testing capabilities

--- Week 4/Kahoot/server/model.js
+++ Week 4/Kahoot/server/model.js
@@ -5,85 +5,90 @@ const bcrypt = require("bcrypt");
 dotenv.config();
 mongoose.connect(process.env.DB_LINK);
 
-
-const QuestionSchema = new mongoose.Schema ({
-    text: {
+const QuestionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, "This is not a valid question"],
+  },
+  answers: [
+    {
+      answerText: {
         type: String,
-        required: [true, "This is not a valid question"]
+        required: [true, "Answer must have text"],
+      },
+      isTrue: {
+        type: Boolean,
+        required: true,
+      },
     },
-    answers: [{
-        answerText: {
-            type: String,
-            required: [true, "Answer must have text"]
-        },
-        isTrue: {
-            type: Boolean,
-            required: true
-        }
-    }]
+  ],
 });
 
 const QuizSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, "Quiz must have a title"]
-    },
-    description: String,
-    questions: [{type: mongoose.Schema.Types.ObjectId, ref: "Question"}],
+  title: {
+    type: String,
+    required: [true, "Quiz must have a title"],
+  },
+  description: String,
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Question" }],
 });
-
 
 const UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: [true, "You must enter a name"]
-    },
-    lastName: {
-        type: String,
-        required: [true, "You must enter a last name"]
-    },
-    email: {
-        type: String,
-        required: [true, "E-Mail address is reqired"],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true , "Password Required"]
-    }
+  firstName: {
+    type: String,
+    required: [true, "You must enter a name"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "You must enter a last name"],
+  },
+  email: {
+    type: String,
+    required: [true, "E-Mail address is reqired"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password Required"],
+  },
 });
 
-UserSchema.methods.setPassword = function(plainPassword) {
-    var promise = new Promise((resolve, reject) => {
-        bcrypt.hash(plainPassword, 12).then(hashedPassword => {
-            this.password = hashedPassword;
-            resolve();
-        }).catch(() => {
-            reject();
-        });
-    });
+UserSchema.methods.setPassword = function (plainPassword) {
+  var promise = new Promise((resolve, reject) => {
+    bcrypt
+      .hash(plainPassword, 12)
+      .then((hashedPassword) => {
+        this.password = hashedPassword;
+        resolve();
+      })
+      .catch(() => {
+        reject();
+      });
+  });
 
-    return promise;
-
+  return promise;
 };
 
-UserSchema.methods.verifyPassword = function(plainPassword) {
-    var promise = new Promise((resolve, reject) => {
-        bcrypt.compare(plainPassword, this.password).then(result => {
-            resolve(result);
-        }).catch(() => {
-            reject();
-        });
-    });
-    return promise;
+UserSchema.methods.verifyPassword = function (plainPassword) {
+  var promise = new Promise((resolve, reject) => {
+    bcrypt
+      .compare(plainPassword, this.password)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch(() => {
+        reject();
+      });
+  });
+  return promise;
 };
 
-const Quiz = mongoose.model("Quiz" , QuizSchema);
-const Question = mongoose.model("Question" , QuestionSchema);
-const User = mongoose.model("User" , UserSchema);
-const RedactedUser = mongoose.model("RedactedUser" , UserSchema);
+const Quiz = mongoose.model("Quiz", QuizSchema);
+const Question = mongoose.model("Question", QuestionSchema);
+const User = mongoose.model("User", UserSchema);
+const RedactedUser = mongoose.model("RedactedUser", UserSchema);
 
-User.createCollection()
+User.createCollection();
 
 // RedactedUser.createCollection({
 //     viewOn: "users",
@@ -97,8 +102,8 @@ User.createCollection()
 // })
 
 module.exports = {
-    Quiz : Quiz,
-    Question : Question,
-    User : User,
-    RedactedUser: RedactedUser,
+  Quiz: Quiz,
+  Question: Question,
+  User: User,
+  RedactedUser: RedactedUser,
 };
